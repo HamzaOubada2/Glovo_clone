41ed713482c719b41ec12270ef1e9a57d9c152b2
HamzaOubada2 <OubHam220@gmail.com>
2026-09-10 01:58:53 +0100

    Refactor code structure for improved readability and maintainability

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (ConfigService: ConfigService) => ({
+                type: 'mysql',
+                host: ConfigService.get<string>('DB_HOST'),
+                port: ConfigService.get<number>('DB_PORT'),
+                username: ConfigService.get<string>('DB_USER'),
+                password: ConfigService.get<string>('DB_PASSWORD'),
+                database: ConfigService.get<string>("DB_NAME"),
+                entities: [],
+                synchronize: true
+            })
+        })
+    ]
 })
 export class AppModule {}
